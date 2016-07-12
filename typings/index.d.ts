1031901ca11b57674a0bb39da622f06a5ff78e2e
GreenDou <zn.he@foxmail.com>
2016-07-12 20:44:19 +0800

    Add fabric.js in typings.

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,4 +1,5 @@
 /// <reference path="globals/eventemitter3/index.d.ts" />
+/// <reference path="globals/fabricjs/index.d.ts" />
 /// <reference path="globals/react-dom/index.d.ts" />
 /// <reference path="globals/react/index.d.ts" />
 /// <reference path="modules/flux/index.d.ts" />
